52d1b6cb72e6b1bce64dfb2f7ece60f259fa09f2
VoidGravity <abdellahbardichwork@gmal.com>
2023-12-20 09:22:43 +0100

    trying ot recover files to fix offer page

--- configs/app.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('ABS_URL', 'http://localhost/php_brief/oopPHP');
-define('ABS_URL_dash','http://localhost/php_brief/oopPHP/assets/styles/dashboard/');
-define('ABS_IMG','C:/xampp/htdocs/php_brief/oopPHP/assets/styles/dashboard/');
-// define ('ABS_URL_login','http://localhost/php_brief/oopPHP/assets/styles/loginstyle.css');
